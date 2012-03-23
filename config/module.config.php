45645de49db8fb4df33726994ad4fe6b8a1104a4
Ben Youngblood <bx.youngblood@gmail.com>
2012-03-23 16:32:12 -0700

    DI configuration

--- config/module.config.php
+++ config/module.config.php
@@ -8,8 +8,42 @@ return array(
         'instance' => array(
             'alias' => array(
                 'edpdiscuss' => 'EdpDiscuss\Controller\IndexController',
-            ),
 
+                // Default Zend\Db
+                'edpdiscuss_zend_db_adapter'    => 'Zend\Db\Adapter\Adapter',
+                'edpdiscuss_thread_mapper'      => 'EdpDiscuss\Model\Thread\ThreadMapper',
+                'edpdiscuss_message_mapper'     => 'EdpDiscuss\Model\Message\MessageMapper',
+                'edpdiscuss_thread_tg'          => 'Zend\Db\TableGateway\TableGateway',
+                'edpdiscuss_message_tg'         => 'Zend\Db\TableGateway\TableGateway',
+            ),
+            'EdpDiscuss\Service\Discuss' => array(
+                'parameters' => array(
+                    'threadMapper'  => 'edpdiscuss_thread_mapper',
+                    'messageMapper' => 'edpdiscuss_message_mapper',
+                )
+            ),
+            'EdpDiscuss\Model\Thread\ThreadMapper' => array(
+                'parameters' => array(
+                    'tableGateway' => 'edpdiscuss_thread_tg'
+                ),
+            ),
+            'EdpDiscuss\Model\Message\MessageMapper' => array(
+                'parameters' => array(
+                    'tableGateway' => 'edpdiscuss_message_tg'
+                ),
+            ),
+            'edpdiscuss_thread_tg' => array(
+                'parameters' => array(
+                    'tableName' => 'discuss_thread',
+                    'adapter'   => 'edpdiscuss_zend_db_adapter',
+                ),
+            ),
+            'edpdiscuss_message_tg' => array(
+                'parameters' => array(
+                    'tableName' => 'discuss_message',
+                    'adapter'   => 'edpdiscuss_zend_db_adapter',
+                ),
+            ),
             'Zend\View\Resolver\TemplatePathStack' => array(
                 'parameters' => array(
                     'paths'  => array(
