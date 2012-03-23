f1bea17eefb3d1584cf439299dd928f82c160268
Ben Youngblood <bx.youngblood@gmail.com>
2012-03-23 16:46:35 -0700

    use DI to get service in controller

--- src/EdpDiscuss/Controller/IndexController.php
+++ src/EdpDiscuss/Controller/IndexController.php
@@ -3,12 +3,43 @@
 namespace EdpDiscuss\Controller;
 
 use Zend\Mvc\Controller\ActionController,
-    Zend\View\Model\ViewModel;
+    Zend\View\Model\ViewModel,
+    EdpDiscuss\Service\Discuss as DiscussService;
 
 class IndexController extends ActionController
 {
+
+    /**
+     * discussService
+     * 
+     * @var Discuss
+     */
+    protected $discussService;
+
     public function indexAction()
     {
+        var_dump($this->discussService->getLatestThreads());
         return new ViewModel();
+    }
+ 
+    /**
+     * Get discussService.
+     *
+     * @return discussService
+     */
+    public function getDiscussService()
+    {
+        return $this->discussService;
+    }
+ 
+    /**
+     * Set discussService.
+     *
+     * @param $discussService the value to be set
+     */
+    public function setDiscussService($discussService)
+    {
+        $this->discussService = $discussService;
+        return $this;
     }
 }
