af5181ae6c99ae01650426f1a2dc1170fefd4441
Ben Youngblood <bx.youngblood@gmail.com>
2012-03-23 16:07:34 -0700

    implement basic message mapper methods

--- src/EdpDiscuss/Model/Message/MessageMapper.php
+++ src/EdpDiscuss/Model/Message/MessageMapper.php
@@ -7,17 +7,61 @@ use ZfcBase\Mapper\DbMapperAbstract;
 class MessageMapper extends DbMapperAbstract implements MessageMapperInterface
 {
     protected $tableName = 'discuss_message';
+    protected $messageIDField = 'message_id';
+    protected $threadIDField = 'thread_id';
 
     /**
-     * getThreadMessages 
-     * 
+     * getMessageById
+     *
+     * @param int $messageId
+     * @return MessageInterface
+     */
+    public function getMessageById($messageId)
+    {
+        $rowset = $this->getTableGateway()->select(array($this->messageIDField => $messageId));
+        $row = $rowset->current();
+
+        $messageClass = EdpDiscuss::getOption('message_model_class');
+        $message = $messageClass::fromArray($row);
+
+        return $message;
+    }
+
+    /**
+     * getMessagesByThread 
+     *
      * @param int $threadId 
      * @param int $limit 
      * @param int $offest 
      * @return array of EdpDiscuss\Model\Message\MessageInterface's
      */
-    public function getMessageByThread($threadId, $limit = 25, $offest = 0)
+    public function getMessageByThread($threadId, $limit = 25, $offset = 0)
     {
+        $rowset = $this->getTableGateway()->select(array($this->threadIDField => $threadId));
 
+        $messageClass = EdpDiscuss::getOption('message_model_class');
+        $messages = $messageClass::fromArraySet($rowset);
+
+        return $messages;
+    }
+
+    /**
+     * persist
+     *
+     * @param MessageInterface $message
+     * @return MessageInterface
+     */
+    public function persist(MessageInterface $message)
+    {
+        $data = new ArrayObject($message->toArray());
+        if ($message->getMessageId() > 0) {
+            $this->getTableGateway()->update((array) $data, array($this->messageIDField => $message->getMessageId()));
+        } else {
+            $this->getTableGateway()->insert((array) $data);
+            $messageId = $this->getTableGateway()->getAdapter()->getDriver()->getConnection()->getLastGeneratedId();
+            $message->setMessageId($messageId);
+        }
+
+        return $message;
     }
 }
--- src/EdpDiscuss/Model/Message/MessageMapperInterface.php
+++ src/EdpDiscuss/Model/Message/MessageMapperInterface.php
@@ -5,6 +5,14 @@ namespace EdpDiscuss\Model\Message;
 interface MessageMapperInterface
 {
     /**
+     * getMessageById
+     * 
+     * @param int $messageId
+     * @return MessageInterface
+     */
+    public function getMessageById($messageId);
+
+    /**
      * getMessagesByThread
      * 
      * @param int $threadId 
@@ -12,5 +20,13 @@ interface MessageMapperInterface
      * @param int $offest 
      * @return array of EdpDiscuss\Model\Message\MessageInterface's
      */
-    public function getMessagesByThread($threadId, $limit = 25, $offest = 0);
+    public function getMessagesByThread($threadId, $limit = 25, $offset = 0);
+
+    /**
+     * persist 
+     * 
+     * @param MessageInterface $message 
+     * @return MessageInterface
+     */
+    public function persist(MessageInterface $message);
 }
