3eb5e7394c14afbfbe8e10626aab898da72b9be0
Ben Youngblood <bx.youngblood@gmail.com>
2012-03-23 13:38:46 -0700

    setup views (for development purposes)

--- config/module.config.php
+++ config/module.config.php
@@ -5,11 +5,92 @@ return array(
             'alias' => array(
                 'edpdiscuss' => 'EdpDiscuss\Controller\IndexController',
             ),
+
+            // Setup for router and routes
+            'Zend\Mvc\Router\RouteStack' => array(
+                'parameters' => array(
+                    'routes' => array(
+                        'default' => array(
+                            'type'    => 'Zend\Mvc\Router\Http\Segment',
+                            'options' => array(
+                                'route'    => '/[:controller[/:action]]',
+                                'constraints' => array(
+                                    'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                    'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                ),
+                                'defaults' => array(
+                                    'controller' => 'EdpDiscuss\Controller\IndexController',
+                                    'action'     => 'index',
+                                ),
+                            ),
+                        ),
+                        'home' => array(
+                            'type' => 'Zend\Mvc\Router\Http\Literal',
+                            'options' => array(
+                                'route'    => '/',
+                                'defaults' => array(
+                                    'controller' => 'EdpDiscuss\Controller\IndexController',
+                                    'action'     => 'index',
+                                ),
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+
+            // Using the PhpRenderer, which just handles html produced by php 
+            // scripts
+            'Zend\View\Renderer\PhpRenderer' => array(
+                'parameters' => array(
+                    'resolver' => 'Zend\View\Resolver\AggregateResolver',
+                ),
+            ),
+            // Defining how the view scripts should be resolved by stacking up
+            // a Zend\View\Resolver\TemplateMapResolver and a
+            // Zend\View\Resolver\TemplatePathStack
+            'Zend\View\Resolver\AggregateResolver' => array(
+                'injections' => array(
+                    'Zend\View\Resolver\TemplatePathStack',
+                ),
+            ),
             'Zend\View\Resolver\TemplatePathStack' => array(
                 'parameters' => array(
                     'paths'  => array(
                         'edpdiscuss' => __DIR__ . '/../view',
                     ),
+                ),
+            ),
+            // View for the layout
+            'Zend\Mvc\View\DefaultRenderingStrategy' => array(
+                'parameters' => array(
+                    'layoutTemplate' => 'layout/layout',
+                ),
+            ),
+            // Injecting the router into the url helper
+            'Zend\View\Helper\Url' => array(
+                'parameters' => array(
+                    'router' => 'Zend\Mvc\Router\RouteStack',
+                ),
+            ),
+            // Configuration for the doctype helper
+            'Zend\View\Helper\Doctype' => array(
+                'parameters' => array(
+                    'doctype' => 'HTML5',
+                ),
+            ),
+            // View script rendered in case of 404 exception
+            'Zend\Mvc\View\RouteNotFoundStrategy' => array(
+                'parameters' => array(
+                    'displayNotFoundReason' => true,
+                    'displayExceptions'     => true,
+                    'notFoundTemplate'      => 'error/404',
+                ),
+            ),
+            // View script rendered in case of other exceptions
+            'Zend\Mvc\View\ExceptionStrategy' => array(
+                'parameters' => array(
+                    'displayExceptions' => true,
+                    'exceptionTemplate' => 'error/index',
                 ),
             ),
         ),
